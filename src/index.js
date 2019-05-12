6bf96910ebeaae94502d43a49e4beef545da008d
Payam Rastogi <payamrastogi@gmail.com>
2019-05-12 17:06:03 -0400

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+function Hi() {
+  return <div>Hello! World</div>;
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Hi />, document.querySelector("#root"));

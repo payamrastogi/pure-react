98a45830f8f83b05b85468180b4d3f61dc7dda2c
Payam Rastogi <payamrastogi@gmail.com>
2019-05-14 15:38:01 -0400

    Added func.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MediaCard from './mediaCard';
-import Gate from './gate';
 
-function Hi() {
-  return (
-    <div>
-      Hello! <strong>Payam</strong>
-      {5 + 7}
-    </div>
-  );
+function Room() {
+  return <div className="room">the room is lit</div>;
 }
 
-function Func1(props) {
-  return <div>Func1 {props.name}</div>;
-}
-
-const Func2 = function(props) {
-  return <div>Func2 {props.name}</div>;
-};
-
-const Func3 = props => {
-  return <div>Func3 {props.name}</div>;
-};
-
-const Func4 = props => <div>Func4 {props.name}</div>;
-
-/*function MediaCard(props) {
-  return (
-    <div>
-      <h2>{props.title}</h2>
-      <p>{props.body}</p>
-      <img src={props.imageUrl} />
-    </div>
-  );
-}*/
-
-//const Gate = props => <div>{props.isOpen ? "Open" : "Closed"}</div>;
-
-//ReactDOM.render(<Func4 name="Payam"/>, document.querySelector("#root"));
-//ReactDOM.render(<MediaCard title="media card" body={<b>Test Body</b>} imageUrl="http://www.google.com"/>, document.querySelector("#root"));
-ReactDOM.render(<Gate isOpen={false} />, document.querySelector("#root"));
+ReactDOM.render(<Room />, document.getElementById("root"));
